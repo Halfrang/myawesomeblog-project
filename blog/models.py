0805c06293f42e3151e7a51397f5a7c6254ec580
Тэн Александр Михайлович <a.ten@gki.rsa.rogsibal.ru>
2020-10-06 17:34:08 +0300

    Blog Added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class post(models.Model):
+	title = models.CharField(max_length=300, default=' ')
+	date = models.DateTimeField()
+	text = models.TextField(default=' ')
+	post_image = models.ImageField(upload_to='blog_images/')
